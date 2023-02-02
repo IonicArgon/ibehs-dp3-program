79bc0a95fb9cf00ccc792aa0396bdbc13b33eab1
emilyattai <attaie@mcmaster.ca>
2023-02-02 13:30:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from hardware.orientation import Orientation
-from hardware.steppers import Steppers 
-from hardware.vibration import Vibration
-
 if __name__ == "__main__":
     print("Hello World!")
+
+
+
+
