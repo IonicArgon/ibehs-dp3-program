d36ae97b43aa3ff28713d9bba8cffd7d326ad11f
Marco Tan <marco.tan.200405@gmail.com>
2023-02-09 13:44:58 -0500

    orientation sensor code made but not tested

--- hardware/orientation.py
+++ hardware/orientation.py
@@ -1,5 +1,29 @@
 # space for imports later
+import asyncio
+from lib.sensor_library import Orientation_Sensor
+from lib.ema import EMA
 
 class Orientation():
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, p_alpha, p_window_size, p_round):
+        self.m_sensor = Orientation_Sensor()
+        self.m_ema_xyz = [
+            EMA(p_alpha, p_window_size, p_round),
+            EMA(p_alpha, p_window_size, p_round),
+            EMA(p_alpha, p_window_size, p_round)
+        ]
+        self.m_raw = (None, None, None)
+        self.m_ema_out = (None, None, None)
+        self.m_lock = asyncio.Lock()
+
+    async def get(self):
+        async with self.m_lock:
+            return self.m_ema_out
+
+    async def update(self):
+        while True:
+            async with self.m_lock:
+                self.m_raw = self.m_sensor.euler_angles()
+                for i in range(3):
+                    self.m_ema_xyz[i].update(self.m_raw[i])
+                    self.m_ema_out = (self.m_ema_xyz[0].get(), self.m_ema_xyz[1].get(), self.m_ema_xyz[2].get())
+            await asyncio.sleep(0.1)
--- main.py
+++ main.py
@@ -2,37 +2,7 @@ from hardware.orientation import Orientation
 from hardware.steppers import Steppers 
 from hardware.vibration import Vibration
 
-# for testing
-from lib.ema import EMA
-import matplotlib.pyplot as plt
-import numpy as np
-from collections import deque
+import asyncio
 
-def test():
-    ema = EMA(0.9, 10, 2)
-
-    y = 0.0
-    y_list = deque(maxlen=100)
-    ema_list = deque(maxlen=100)
-
-    while True:
-        try:
-            y_list.append(y)
-            ema.update(y)
-            ema_list.append(ema.get())
-            y += np.random.normal(0.0, 0.25)
-
-            plt.plot(y_list, label="y")
-            plt.plot(ema_list, label="ema")
-            plt.scatter(range(len(y_list)), y_list)
-            plt.scatter(range(len(ema_list)), ema_list)
-            plt.legend()
-
-            plt.draw()
-            plt.pause(0.001)
-            plt.clf()
-        except KeyboardInterrupt:
-            break
-
-if __name__ == "__main__":
+async def main():
     raise NotImplementedError
