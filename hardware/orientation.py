9cf46aec903910162a7c4229ab2bb221442559b7
Marco Tan <marco.tan.200405@gmail.com>
2023-02-09 18:28:17 -0500

    without lock

--- hardware/orientation.py
+++ hardware/orientation.py
@@ -13,22 +13,22 @@ class Orientation():
         ]
         self.m_raw = (None, None, None)
         self.m_ema_out = (None, None, None)
-        self.m_lock = asyncio.Lock()
+        # self.m_lock = asyncio.Lock()
 
     async def get(self):
-        print("getting orientation")
-        async with self.m_lock:
-            print("orientation locked")
-            return self.m_ema_out
+        # print("getting orientation")
+        # async with self.m_lock:
+        print("orientation locked")
+        return self.m_ema_out
 
     async def update(self):
         while True:
-            print("updating orientation")
-            async with self.m_lock:
-                print("orientation locked")
-                self.m_raw = self.m_sensor.euler_angles()
-                for i in range(3):
-                    self.m_ema_xyz[i].update(self.m_raw[i])
-                    self.m_ema_out = (self.m_ema_xyz[0].get(), self.m_ema_xyz[1].get(), self.m_ema_xyz[2].get())
+            # print("updating orientation")
+            # async with self.m_lock:
+            print("orientation locked")
+            self.m_raw = self.m_sensor.euler_angles()
+            for i in range(3):
+                self.m_ema_xyz[i].update(self.m_raw[i])
+                self.m_ema_out = (self.m_ema_xyz[0].get(), self.m_ema_xyz[1].get(), self.m_ema_xyz[2].get())
             print("orientation unlocked")
             await asyncio.sleep(0.1)
