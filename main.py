c22c2aaf16eab6ff36c0c514b25884fb7cad61c3
Marco Tan <marco.tan.200405@gmail.com>
2023-02-09 19:30:55 -0500

    1 subplot test

--- main.py
+++ main.py
@@ -21,34 +21,24 @@ def main():
     ema_raw_y = deque(maxlen=25)
     ema_raw_z = deque(maxlen=25)
 
-    fig, ax = plt.subplots(3)
+    fig, ax = plt.subplots(1)
     fig.suptitle("Orientation Data")
 
     while True:
         ema_out = orientation.get()
-        ema_out_x.append(float(ema_out[0] or 0.0))
-        ema_out_y.append(float(ema_out[1] or 0.0))
-        ema_out_z.append(float(ema_out[2] or 0.0))
+        ema_out_x.append(ema_out[0])
+        ema_out_y.append(ema_out[1])
+        ema_out_z.append(ema_out[2])
 
         ema_raw = orientation.get_raw()
-        ema_raw_x.append(float(ema_raw[0] or 0.0))
-        ema_raw_y.append(float(ema_raw[1] or 0.0))
-        ema_raw_z.append(float(ema_raw[2] or 0.0))
+        ema_raw_x.append(ema_raw[0])
+        ema_raw_y.append(ema_raw[1])
+        ema_raw_z.append(ema_raw[2])
 
         ax[0].plot(ema_out_x, label="EMA")
         ax[0].plot(ema_raw_x, label="Raw")
         ax[0].set_title("X")
         ax[0].legend()
-
-        ax[1].plot(ema_out_y, label="EMA")
-        ax[1].plot(ema_raw_y, label="Raw")
-        ax[1].set_title("Y")
-        ax[1].legend()
-
-        ax[2].plot(ema_out_z, label="EMA")
-        ax[2].plot(ema_raw_z, label="Raw")
-        ax[2].set_title("Z")
-        ax[2].legend()
 
         plt.draw()
         plt.pause(0.01)
