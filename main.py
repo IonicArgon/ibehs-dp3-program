a150bf558f63cac032c8cf81de32f99352d9ccf7
Marco Tan <marco.tan.200405@gmail.com>
2023-02-09 19:22:27 -0500

    this test code just looks prettier honestly

--- main.py
+++ main.py
@@ -14,9 +14,15 @@ from collections import deque
 
 def main():
     orientation = Orientation(0.9, 10, 2)
-    ema_out_x = deque(maxlen=100)
-    ema_out_y = deque(maxlen=100)
-    ema_out_z = deque(maxlen=100)
+    ema_out_x = deque(maxlen=25)
+    ema_out_y = deque(maxlen=25)
+    ema_out_z = deque(maxlen=25)
+    ema_raw_x = deque(maxlen=25)
+    ema_raw_y = deque(maxlen=25)
+    ema_raw_z = deque(maxlen=25)
+
+    fig, ax = plt.subplots(3)
+    fig.suptitle("Orientation Data")
 
     while True:
         ema_out = orientation.get()
@@ -24,18 +30,29 @@ def main():
         ema_out_y.append(ema_out[1])
         ema_out_z.append(ema_out[2])
 
-        plt.plot(ema_out_x, label="x")
-        plt.plot(ema_out_y, label="y")
-        plt.plot(ema_out_z, label="z")
-        plt.scatter(range(len(ema_out_x)), ema_out_x)
-        plt.scatter(range(len(ema_out_y)), ema_out_y)
-        plt.scatter(range(len(ema_out_z)), ema_out_z)
-        plt.legend(loc="upper left")
+        ema_raw = orientation.get_raw()
+        ema_raw_x.append(ema_raw[0])
+        ema_raw_y.append(ema_raw[1])
+        ema_raw_z.append(ema_raw[2])
+
+        ax[0].plot(ema_out_x, label="EMA")
+        ax[0].plot(ema_raw_x, label="Raw")
+        ax[0].set_title("X")
+        ax[0].legend()
+
+        ax[1].plot(ema_out_y, label="EMA")
+        ax[1].plot(ema_raw_y, label="Raw")
+        ax[1].set_title("Y")
+        ax[1].legend()
+
+        ax[2].plot(ema_out_z, label="EMA")
+        ax[2].plot(ema_raw_z, label="Raw")
+        ax[2].set_title("Z")
+        ax[2].legend()
 
         plt.draw()
         plt.pause(0.01)
         plt.clf()
-        print("plot updated")
 
 if __name__ == "__main__":
     main()
