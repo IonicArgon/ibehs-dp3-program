ad81bdb8ade73f4bf8448c625c87871f0840f117
Marco Tan <marco.tan.200405@gmail.com>
2023-02-09 19:14:18 -0500

    try without lock

--- hardware/orientation.py
+++ hardware/orientation.py
@@ -14,25 +14,21 @@ class Orientation():
         ]
         self.m_raw = (None, None, None)
         self.m_ema_out = (None, None, None)
-        self.m_lock = threading.Lock()
+        # self.m_lock = threading.Lock()
 
         thread = threading.Thread(target=self.update)
         thread.start()
 
     def get(self):
         print("getting orientation")
-        self.m_lock.acquire()
-        yield self.m_ema_out
-        self.m_lock.release()
+        return self.m_ema_out
 
     def update(self):
         while True:
             print("updating orientation")
-            self.m_lock.acquire()
             self.m_raw = self.m_sensor.euler_angles()
             for i in range(3):
                 self.m_ema_xyz[i].update(self.m_raw[i])
                 self.m_ema_out = (self.m_ema_xyz[0].get(), self.m_ema_xyz[1].get(), self.m_ema_xyz[2].get())
-            self.m_lock.release()
             time.sleep(0.1)
 
