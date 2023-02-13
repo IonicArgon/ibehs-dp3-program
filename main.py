4c8add2f4a7f5f9a17ad629988d10337e22a3dd1
emilyattai <attaie@mcmaster.ca>
2023-02-13 12:39:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,35 @@ if __name__ == "__main__":
 
 
 
+from gpiozero import Buzzer
+import sys 
+import logging
+import threading
+import time
 
+#boundaries 
+move_forward = #need to test to find these values 
+move_backward =
+move_right =
+move_left =
+move_stop = 
+
+class US_Al: 
+    def user_alert:
+        logging.info()
+        while True:
+            if head_position == move_forward:
+                vibration_motor.off()
+            elif head_position == move_backward:
+                vibration_motor.off()
+            elif head_position == move_right:
+                vibration_motor.off()
+            elif head_position == move_left:
+                vibration_motor.off()
+            elif head_position == move_stop:
+                vibration_motor.off()
+            else: 
+                vibration_motor.on()
+                    
+alert = threading.Thread(target = user_alert, args=(1,))
+alert.start()
