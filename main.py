7cfb52bde7585831aa870dd4a0c0f2c56737667d
Marco Tan <marco.tan.200405@gmail.com>
2023-02-16 12:50:29 -0500

    bruh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from hardware.orientation import Orientation
-from hardware.steppers import Steppers 
-from hardware.vibration import Vibration
+# from hardware.steppers import Steppers 
+# from hardware.vibration import Vibration
 from hardware.vibration import Head_Position
 
 # important imports
@@ -9,9 +9,11 @@ import time
 import sys
 
 def main():
-    # orientation = Orientation(0.9, 10, 2)
-    # vibration = Vibration(17)
-    ...
+    orientation = Orientation(0.9, 10, 2)
+
+    while True:
+        print(orientation.get())
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
