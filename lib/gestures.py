e984377145bf07406c42009c35b1892cc5a6ee1c
Marco Tan <marco.tan.200405@gmail.com>
2023-02-23 14:19:14 -0500

    probably doesn't work but we need to test it

--- lib/gestures.py
+++ lib/gestures.py
@@ -3,6 +3,7 @@ from enum import IntEnum
 import threading
 import time
 import json
+import math
 
 class Head_Position(IntEnum):
     MOVE_FORWARD    = 0
@@ -27,6 +28,10 @@ class Gestures():
         # internal values
         self.m_head_position = Head_Position.MOVE_STOP
         self.m_internal_orientation = None
+        self.m_internal_xyz = [0, 0, 0]
+        self.m_largest_direction_xyz = [0, 0, 0]
+        self.m_prev_vector = 0
+        self.m_count = 0
 
         # for threading
         self.m_update_internal_thread = threading.Thread(target=self.update_internal_values)
@@ -46,13 +51,40 @@ class Gestures():
     def get(self):
         return self.m_head_position
 
+    def set_xyz(self, p_xyz):
+        self.m_p_xyz = p_xyz
+
     def update_internal_values(self):
         while True:
             if self.m_update_internal_thread_running == False:
                 self.m_internal_orientation = None
+                self.m_largest_direction_xyz = [0, 0, 0]
+                self.m_prev_vector = 0
+                self.m_count = 0
                 pass
             else:
-                raise NotImplementedError("update_internal_values() not implemented")
+                for i in range(1, 3):
+                    vector = self.m_p_xyz[i]
+                    if abs(vector) > abs(self.m_prev_vector):
+                        self.m_largest_direction_xyz = [0, 0, 0]
+                        self.m_largest_direction_xyz[i] = 1
+                        self.m_prev_vector = vector
+
+                for i in self.m_gestures:
+                    gesture = self.m_gestures[i]
+                    direction_test = (gesture["direction"] == self.m_largest_direction_xyz)
+                    threshold_test = (abs(self.m_p_xyz[gesture["direction"].index(1)]) > abs(gesture["threshold"]))
+                    
+                    if direction_test and threshold_test:
+                        # only increment on rising edge case of threshold test
+                        if self.m_prev_vector < gesture["threshold"]:
+                            self.m_count += 1
+                        break
+
+                self.m_internal_orientation = {
+                    "direction": self.m_largest_direction_xyz,
+                    "count": self.m_count
+                }
             time.sleep(0.1)
 
     def update_gesture_output(self):
