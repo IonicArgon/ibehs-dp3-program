a9d2c9ba71659f7d095aae1f0bc5b490dbc198d0
Marco Tan <marco.tan.200405@gmail.com>
2023-02-24 15:19:30 -0500

    testing with larger value

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def main():
 
 def main():
     test_stepper = Stepper_Driver([0, 5, 6, 13], 0.002, True)
-    test_stepper.step(200)
+    test_stepper.step(2048)
     time.sleep(5)
 
 if __name__ == "__main__":
