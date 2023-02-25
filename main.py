5431016bbb7d1c14fce58d730dbfdd5a0ffbef79
Marco Tan <marco.tan.200405@gmail.com>
2023-02-24 22:27:12 -0500

    the tabs are messing with

--- main.py
+++ main.py
@@ -21,15 +21,16 @@ gestures = Gestures(p_config_file="config.json", p_gesture_window_time=1.0)
 
 def console_output_fn():
     COLUMN_WIDTH = 18
-    COLUMN_THIRD = 6
+    COLUMN_THIRD = int(COLUMN_WIDTH / 3)
 
-    header = f'{"Timestamp":^{COLUMN_WIDTH}}|{"Orientation":^{COLUMN_WIDTH}}|\
-        {"Gestures":^{COLUMN_WIDTH}}|{"Steppers":^{COLUMN_WIDTH}}|\
-        {"Vibration":^{COLUMN_WIDTH}}'
-    subheader = f'{" ":^{COLUMN_WIDTH}}|{"X":^{COLUMN_THIRD}}\
-        {"Y":^{COLUMN_THIRD}}{"Z":^{COLUMN_THIRD}}|{" ":^{COLUMN_WIDTH}}|\
-        {"X":^{COLUMN_THIRD}}{"Z":^{COLUMN_THIRD}}{" ":^{COLUMN_THIRD}}|\
-        {" ":^{COLUMN_WIDTH}}'
+    header = f'{"Timestamp":^{COLUMN_WIDTH}}|{"Orientation":^{COLUMN_WIDTH}}|' + \
+             f'{"Gestures":^{COLUMN_WIDTH}}|{"Steppers":^{COLUMN_WIDTH}}|' + \
+             f'{"Vibration":^{COLUMN_WIDTH}}'
+    subheader = f'{" ":^{COLUMN_WIDTH}}|' + \
+                f'{"X":^{COLUMN_THIRD}}{"Y":^{COLUMN_THIRD}}{"Z":^{COLUMN_THIRD}}|' + \
+                f'{" ":^{COLUMN_WIDTH}}|' + \
+                f'{"X":^{COLUMN_THIRD}}{"Z":^{COLUMN_THIRD}}{" ":^{COLUMN_THIRD}}|' + \
+                f'{" ":^{COLUMN_WIDTH}}'
     header_counter = 0
 
     global orientation
@@ -56,12 +57,11 @@ def console_output_fn():
         # stepper_now = stepper_ctrl.get_status()
         # vibration_now = vibration.get_status()
 
-        outputString = f'{time_now:^{COLUMN_WIDTH}}|\
-            {xyz_now[0]:^{COLUMN_THIRD}}\
-            {xyz_now[1]:^{COLUMN_THIRD}}\
-            {xyz_now[2]:^{COLUMN_THIRD}}|\
-            {gestures_now:^{COLUMN_WIDTH}}|'
-                        #  {stepper_now[0]:^5}{stepper_now[1]:^5}{" ":^5}|{vibration_now:^15}'
+        outputString = f'{time_now:^{COLUMN_WIDTH}}|' + \
+                       f'{xyz_now[0]:^{COLUMN_THIRD}}|' + \
+                       f'{xyz_now[1]:^{COLUMN_THIRD}}|' + \
+                       f'{xyz_now[2]:^{COLUMN_THIRD}}||' + \
+                       f'{gestures_now:^{COLUMN_WIDTH}}||' + \
         print(outputString)
         header_counter += 1
         time.sleep(1)
